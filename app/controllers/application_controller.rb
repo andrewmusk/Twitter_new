49bd6d7439502372aae8ea9dd245c05f4d5a8b0f
andrewmusk <andrew@ixperience.co.za>
2016-03-03 22:19:54 +0200

    Setup new fields in devise user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :username, :email, :password, :password_confirmation,:phone_number,:date_of_birth,:user_bio,:country,:is_female)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303195028) do
+ActiveRecord::Schema.define(version: 20160303201059) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "name"
+    t.string   "username"
+    t.string   "phone_number"
+    t.datetime "date_of_birth"
+    t.string   "user_bio"
+    t.integer  "followers"
+    t.integer  "following"
+    t.boolean  "is_female",              default: false
+    t.string   "country"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
